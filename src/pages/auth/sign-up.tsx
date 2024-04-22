78b807e5aa633c59a8d8314d278e83fa89e3978d
souph1s <sophiamperez@hotmail.com>
2024-04-22 20:45:05 -0300

    feat: create new restaurant page

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,112 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('New restaurant created!', {
+        action: {
+          label: 'Login',
+          onClick: () => {
+            navigate('/sign-in')
+          },
+        },
+      })
+    } catch {
+      toast.error('Something went wrong.')
+    }
+  }
+  return (
+    <>
+      <Helmet title="Register" />
+      <div className="p-8">
+        <Button
+          variant={'secondary'}
+          asChild
+          className="absolute right-8 top-8"
+        >
+          <Link to="/sign-in">Log in</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create Free Account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Be a partner and start to track your sales!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="restaurantName">Restaurant name</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('restaurantName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="managerName">Your name</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register('managerName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Your E-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="phone">Your phone number</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Sign Up
+            </Button>
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              By clicking Sign Up, you agree to our{' '}
+              <a className="underline-offset underline" href="#">
+                Terms
+              </a>{' '}
+              and{' '}
+              <a className="underline-offset underline" href="#">
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
