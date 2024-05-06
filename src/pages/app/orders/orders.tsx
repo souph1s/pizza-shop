24b4ce8e653f2a4763351ef276025a3227a8c14b
souph1s <sophiamperez@hotmail.com>
2024-05-06 20:58:58 -0300

    feat: create pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -17,37 +18,38 @@ export function Orders() {
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
 
-                <TableHead className="w-[140px]">ID</TableHead>
+                  <TableHead className="w-[140px]">ID</TableHead>
 
-                <TableHead className="w-[180px]">Created on</TableHead>
+                  <TableHead className="w-[180px]">Created on</TableHead>
 
-                <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
 
-                <TableHead>Customer</TableHead>
+                  <TableHead>Customer</TableHead>
 
-                <TableHead className="w-[140px]">Total order</TableHead>
+                  <TableHead className="w-[140px]">Total order</TableHead>
 
-                <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
 
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
