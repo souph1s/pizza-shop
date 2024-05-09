1588893cf95cc905121ff5ea0fb1c41354ed4a65
souph1s <sophiamperez@hotmail.com>
2024-05-09 00:31:30 -0300

    feat: create page not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Page not found.</h1>
+      <p className="text-accent-foreground">
+        Click to go back to{' '}
+        <Link to="/" className="text-sky-500 dark:text-sky-400">
+          Dashboard
+        </Link>
+        .
+      </p>
+    </div>
+  )
+}
