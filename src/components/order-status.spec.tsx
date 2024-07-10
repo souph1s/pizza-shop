92d9a015bd8acdb8c07ce98a2165491feff172dd
souph1s <sophiamperez@hotmail.com>
2024-07-09 21:26:17 -0300

    unit test: order status

---
+++ src/components/order-status.spec.tsx
@@ -0,0 +1,63 @@
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+  it('should display the right text when order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    const statusText = wrapper.getByText('Pending')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-slate-400')
+  })
+})
+
+describe('Order Status', () => {
+  it('should display the right text when order status is canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />)
+
+    const statusText = wrapper.getByText('Canceled')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-rose-500')
+  })
+})
+
+describe('Order Status', () => {
+  it('should display the right text when order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="delivering" />)
+
+    const statusText = wrapper.getByText('Delivering')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-amber-400')
+  })
+})
+
+describe('Order Status', () => {
+  it('should display the right text when order status is processing', () => {
+    const wrapper = render(<OrderStatus status="processing" />)
+
+    const statusText = wrapper.getByText('Processing')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-amber-400')
+  })
+})
+
+describe('Order Status', () => {
+  it('should display the right text when order status is delivered', () => {
+    const wrapper = render(<OrderStatus status="delivered" />)
+
+    const statusText = wrapper.getByText('Delivered')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-emerald-500')
+  })
+})
